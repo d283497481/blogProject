c3e5063eadd0028482a8aae4914d49601916ee7b
p_plcding <dinglc@tcfuture.tech>
2022-01-18 16:53:51 +0800

    ff

--- nest-server/src/config/database.ts
+++ nest-server/src/config/database.ts
@@ -1,12 +1,12 @@
 import { join } from 'path';
 export default {
   type: 'mysql',
-  // host: 'localhost',
-  host: '127.0.0.1',
+  host: 'localhost',
+  // host: '127.0.0.1',
   port: 3306,
   username: 'root',
-  password: 'root',
-  database: 'blog',
+  password: 'ding888888',
+  database: 'blog_api',
   entities: [join(__dirname, '../', '**/**.entity{.ts,.js}')], // 映射mysql的实体类的文件
   autoLoadEntities: true,
   synchronize: true,
